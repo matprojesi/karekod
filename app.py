f0fe4e71d95806b1affc3eb5b8f6a9f02dd8e81f
matprojesi <matprojetubitak@gmail.com>
2026-05-17 22:22:09 +0300

    Update application title and description in app.py

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ def sifre_coz(sifreli_dize):
     return " ".join(cozulmus_kelimeler)
 
 # --- WEB ARAYÜZÜ (TASARIM) ---
-st.title("🔐 Kare Şifreleme Uygulaması")
-st.markdown("Cümlelerinizi girin, sistem harflerin karelerini alarak şifrelesin! *(Örn: Merhaba Dünya)*")
+st.title("🔐 KAREKOD Şifreleme ve Şifre Çözme Uygulaması")
+st.markdown("Metninizi girin, sistem harflerin karelerini alarak şifrelesin! *(Örn: Merhaba Dünya)*")
 
 islem = st.radio("İşlem Seçin:", ["Şifrele", "Şifre Çöz"])
 metin_girisi = st.text_input("Buraya yazın:")
